a4a135708a152f0f42d3041e0c37de4423a44678
OUEDRAOGO-Mahamadi <ouedraogomahamadi218@gmail.com>
2024-10-17 16:36:08 +0000

    notif

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -256,7 +256,99 @@ const Header = () => {
                                 </button>
                             )}
                         </div>
-          
+                   <div className="dropdown shrink-0">
+                            <Dropdown
+                                offset={[0, 8]}
+                                placement={`${isRtl ? 'bottom-start' : 'bottom-end'}`}
+                                btnClassName="relative block p-2 rounded-full bg-white-light/40 dark:bg-dark/40 hover:text-primary hover:bg-white-light/90 dark:hover:bg-dark/60"
+                                button={
+                                    <span>
+                                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M19.0001 9.7041V9C19.0001 5.13401 15.8661 2 12.0001 2C8.13407 2 5.00006 5.13401 5.00006 9V9.7041C5.00006 10.5491 4.74995 11.3752 4.28123 12.0783L3.13263 13.8012C2.08349 15.3749 2.88442 17.5139 4.70913 18.0116C9.48258 19.3134 14.5175 19.3134 19.291 18.0116C21.1157 17.5139 21.9166 15.3749 20.8675 13.8012L19.7189 12.0783C19.2502 11.3752 19.0001 10.5491 19.0001 9.7041Z"
+                                                stroke="currentColor"
+                                                strokeWidth="1.5"
+                                            />
+                                            <path d="M7.5 19C8.15503 20.7478 9.92246 22 12 22C14.0775 22 15.845 20.7478 16.5 19" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+                                            <path d="M12 6V10" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+                                        </svg>
+                                        <span className="flex absolute w-3 h-3 ltr:right-0 rtl:left-0 top-0">
+                                            <span className="animate-ping absolute ltr:-left-[3px] rtl:-right-[3px] -top-[3px] inline-flex h-full w-full rounded-full bg-success/50 opacity-75"></span>
+                                            <span className="relative inline-flex rounded-full w-[6px] h-[6px] bg-success"></span>
+                                        </span>
+                                    </span>
+                                }
+                            >
+                                <ul className="!py-0 text-dark dark:text-white-dark w-[300px] sm:w-[350px] divide-y dark:divide-white/10">
+                                    <li onClick={(e) => e.stopPropagation()}>
+                                        <div className="flex items-center px-4 py-2 justify-between font-semibold">
+                                            <h4 className="text-lg">Notifications</h4>
+                                        </div>
+                                    </li>
+                                    {notifications.length > 0 ? (
+                                        <>
+                                            {notifications.map((notification) => {
+                                                return (
+                                                    <li key={notification.id} className="dark:text-white-light/90" onClick={(e) => e.stopPropagation()}>
+                                                        <div className="group flex items-center px-4 py-2">
+                                                            
+                                                            <div className="ltr:pl-3 rtl:pr-3 flex flex-auto">
+                                                                <div className="ltr:pr-3 rtl:pl-3">
+                                                                    <h6
+                                                                        dangerouslySetInnerHTML={{
+                                                                            __html: notification.message,
+                                                                        }}
+                                                                    ></h6>
+                                                                    <span className="text-xs block font-normal dark:text-gray-500">{notification.time}</span>
+                                                                </div>
+                                                                <button
+                                                                    type="button"
+                                                                    className="ltr:ml-auto rtl:mr-auto text-neutral-300 hover:text-danger opacity-0 group-hover:opacity-100"
+                                                                    onClick={() => removeNotification(notification.id)}
+                                                                >
+                                                                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                        <circle opacity="0.5" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="1.5" />
+                                                                        <path d="M14.5 9.50002L9.5 14.5M9.49998 9.5L14.5 14.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+                                                                    </svg>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                );
+                                            })}
+                                            <li>
+                                                <div className="p-4">
+                                                    <button className="btn btn-primary block w-full btn-small">Voir tout</button>
+                                                </div>
+                                            </li>
+                                        </>
+                                    ) : (
+                                        <li onClick={(e) => e.stopPropagation()}>
+                                            <button type="button" className="!grid place-content-center hover:!bg-transparent text-lg min-h-[200px]">
+                                                <div className="mx-auto ring-4 ring-primary/30 rounded-full mb-4">
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        width="40"
+                                                        height="40"
+                                                        viewBox="0 0 24 24"
+                                                        fill="#a9abb6"
+                                                        stroke="#ffffff"
+                                                        strokeWidth="1.5"
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        className="feather feather-info bg-primary rounded-full"
+                                                    >
+                                                        <line x1="12" y1="16" x2="12" y2="12"></line>
+                                                        <line x1="12" y1="8" x2="12.01" y2="8"></line>
+                                                    </svg>
+                                                </div>
+                                                Vous n'avez pas de notification
+                                            </button>
+                                        </li>
+                                    )}
+                                </ul>
+                            </Dropdown>
+                        </div>
                         <div className="dropdown shrink-0 flex">
                             <Dropdown
                                 offset={[0, 8]}
